711e9dfb04d9abaf5f44f3d5f53beb42e3edcceb
Vince Knight <vincent.knight@gmail.com>
2018-06-05 16:11:05 +0100

    Add parameters file. (#18)

---
+++ data/paremeters.py
@@ -0,0 +1,11 @@
+"""
+Parameters used for the numerical experiments
+"""
+import numpy as np
+
+REPETITIONS = 50
+TURNS = 500
+NOISE = 0.05
+PROBEND = 1 / 250
+SEED = 0
+EPSILONS = 10 ** np.linspace(-1, -4, 100)
