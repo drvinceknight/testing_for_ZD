bb1462d509466dba53d08be2de1d202ba5d02a55
Vince Knight <vincent.knight@gmail.com>
2018-06-08 14:38:04 +0100

    Add discussion/ability to measure missing states.

--- src/testzd/main.py
+++ src/testzd/main.py
@@ -11,9 +11,12 @@ def is_delta_ZD(p, rstp=np.array([3, 0, 5, 1]), delta=10 ** (-7)):
                   [P, P, 1]])
     tilde_p = np.array([p[0] - 1, p[1] - 1, p[2], p[3]])
 
-    xbar, residuals, rank, s  = np.linalg.lstsq(M, tilde_p, rcond=None)
+    tilde_p = tilde_p[np.isfinite(p)]
+    M = M[np.isfinite(p)]
 
-    return residuals[0] <= delta
+    xbar = np.linalg.lstsq(M, tilde_p, rcond=None)[0]
+
+    return np.linalg.norm(np.dot(M, xbar) - tilde_p) ** 2 <= delta
 
 def find_lowest_delta(p, step= 10 ** -4):
     """
--- tests/test_is_zd.py
+++ tests/test_is_zd.py
@@ -6,6 +6,10 @@ def test_is_delta_ZD_for_valid_ZD_strategy():
     p = np.array([8 / 9, 1 / 2, 1 / 3, 0])
     assert zd.is_delta_ZD(p) == True
 
+def test_is_delta_ZD_for_valid_ZD_strategy_with_missing_states():
+    p = np.array([8 / 9, np.nan, 1 / 3, np.nan])
+    assert zd.is_delta_ZD(p) == True
+
 def test_is_delta_ZD_for_valid_ZD_strategy_with_different_rstp():
     p = np.array([8 / 9, 1 / 2, 1 / 3, 0])
     rstp = np.array([3, 1, 6, 1])
@@ -15,7 +19,7 @@ def test_is_delta_ZD_for_not_valid_ZD_strategy():
     p = np.array([8 / 9, 1, 1 / 3, 0])
     assert zd.is_delta_ZD(p) == False
 
-def test_is_delta_ZD_for_not_valid_ZD_strategy_with_high_epsilon():
+def test_is_delta_ZD_for_not_valid_ZD_strategy_with_high_delta():
     p = np.array([8 / 9, 1, 1 / 3, 0])
     assert zd.is_delta_ZD(p, delta=10) == True
 
