0776ad635d441290b58d910fbdc08dc3d60e1a5b
Vince Knight <vincent.knight@gmail.com>
2018-06-08 18:04:37 +0100

    Restructure all data.

--- data/raw/main.py
+++ data/raw/main.py
@@ -18,7 +18,7 @@ import pandas as pd
 
 import parameters
 
-def main(players,
+def main(player_group="full",
          tournament_type="std",
          turns=parameters.TURNS,
          noise=parameters.NOISE,
@@ -26,40 +26,41 @@ def main(players,
          repetitions=parameters.REPETITIONS,
          seed=parameters.SEED):
 
-    tournaments = {"std": axl.Tournament(players,
-                                         turns=turns,
-                                         repetitions=repetitions),
-                   "noisy": axl.Tournament(players,
-                                           turns=turns,
-                                           noise=noise,
-                                           repetitions=repetitions),
-                   "probend": axl.Tournament(players,
-                                             prob_end=prob_end,
-                                             repetitions=repetitions)}
+    if player_group == "full":
+        players = [s() for s in axl.strategies
+                   if not s.classifier["long_run_time"]]
+    if player_group == "stewart_plotkin":
+        players = [axl.Cooperator(),
+                   axl.Defector(),
+                   axl.ZDExtort2(),
+                   axl.HardGoByMajority(),
+                   axl.Joss(),
+                   axl.HardTitForTat(),
+                   axl.HardTitFor2Tats(),
+                   axl.TitForTat(),
+                   axl.Grudger(),
+                   axl.GTFT(),
+                   axl.TitFor2Tats(),
+                   axl.WinStayLoseShift(),
+                   axl.Random(),
+                   axl.ZDGTFT2()]
 
-    stewart_and_plotkin_players = [axl.Cooperator(),
-                                   axl.Defector(),
-                                   axl.ZDExtort2(),
-                                   axl.HardGoByMajority(),
-                                   axl.Joss(),
-                                   axl.HardTitForTat(),
-                                   axl.HardTitFor2Tats(),
-                                   axl.TitForTat(),
-                                   axl.Grudger(),
-                                   axl.GTFT(),
-                                   axl.TitFor2Tats(),
-                                   axl.WinStayLoseShift(),
-                                   axl.Random(),
-                                   axl.ZDGTFT2()]
-    full_players = [s() for s in axl.strategies
-                    if not s.classifier["long_run_time"]]
-
-    players_sets = {"stewart_plotkin": stewart_and_plotkin_players,
-                    "full": full_players}
+    if tournament_type == "std":
+        tournament = axl.Tournament(players,
+                                    turns=turns,
+                                    repetitions=repetitions)
+    if tournament_type == "noisy":
+        tournament = axl.Tournament(players,
+                                    turns=turns,
+                                    noise=noise,
+                                    repetitions=repetitions)
+    if tournament_type == "probend":
+        tournament = axl.Tournament(players,
+                                    prob_end=prob_end,
+                                    repetitions=repetitions)
 
     path = pathlib.Path("./")
-    tournament = tournaments[tournament_type]
-    out_path = path / f"./{players}/{tournament_type}"
+    out_path = path / f"./{player_group}/{tournament_type}"
     out_path.mkdir(exist_ok=True, parents=True)
 
     axl.seed(seed)
@@ -70,5 +71,16 @@ def main(players,
 if __name__ == "__main__":
 
     tournament_type = sys.argv[1]
-    players = sys.argv[2]
-    main(players=players, tournament_type=tournament_type)
+    player_group = sys.argv[2]
+    print(f"""
+
+Playing {tournament_type} tournament with {player_group} players
+
+     turns={parameters.TURNS}
+     noise={parameters.NOISE}
+     prob_end={parameters.PROBEND}
+     repetitions={parameters.REPETITIONS}
+     seed={parameters.SEED}
+
+            """)
+    main(player_group=player_group, tournament_type=tournament_type)
