1190241d9f704b0abce6b984e1f7a48263c88562
Vince Knight <vincent.knight@gmail.com>
2018-07-03 19:27:47 +0100

    Add text labels for some strategies for rep plot. (#75)

--- assets/img/replicator_dynamics/main.py
+++ assets/img/replicator_dynamics/main.py
@@ -9,6 +9,17 @@ from scipy.integrate import odeint
 parameters = imp.load_source('parameters',
                              '../../../data/raw/parameters.py')
 
+strategies = [s.name for s in parameters.PLAYER_GROUPS["full"]]
+strategies_of_interest = ["ZD-GTFT-2",
+                          "ZD-GEN-2",
+                          "ZD-Extort-2",
+                          "EvolvedLookerUp2_2_2",
+                          "Evolved ANN 5",
+                          "Tit For Tat",
+                          "Win-Stay Lose-Shift"]
+strategies_of_interest_indices = np.array([i for i, s in enumerate(strategies)
+                                           if s in strategies_of_interest])
+
 def dx(x, t, S):
     """
     Define the derivative of x.
@@ -27,21 +38,44 @@ def main(process_data=False):
             if pair[0] == pair[1]:
                 array[pair] /= 2
 
-        sorted_indices = np.argsort(-np.mean(array, axis=1))
-        sorted_array = array[sorted_indices][:,sorted_indices]
-        np.savetxt("main.csv", sorted_array)
+        np.savetxt("main.csv", array)
     else:
-        sorted_array = np.loadtxt("main.csv")
+        array = np.loadtxt("main.csv")
+
+    sorted_indices = np.argsort(-np.mean(array, axis=1))
+    sorted_array = array[sorted_indices][:,sorted_indices]
+    rank_of_strategies_of_interest = [np.where(sorted_indices == ele)[0][0]
+                                      for ele in strategies_of_interest_indices]
+
 
     ts = np.linspace(0, 5, 10 ** 2)
     x0 = np.array([1 / N for _ in range(N)])
     xs = odeint(func=dx, y0=x0, t=ts, args=(sorted_array,))
 
+
     plt.figure(figsize=(8, 4))
     plt.stackplot(ts, xs.transpose());
     plt.ylabel("Population distribution")
     plt.xlabel("Time units")
-    plt.savefig("main.pdf")
+
+
+    for rank, indx in zip(rank_of_strategies_of_interest,
+                          strategies_of_interest_indices):
+        name = strategies[indx]
+        long_run_prob = xs[-1][rank]
+        y = np.cumsum(xs[-1])[rank] - xs[-1][rank]/2
+        plt.annotate(f"{name}: $x_{{{rank + 1}}}={round(long_run_prob, 3)}$",
+                     xy=(4.95, y),
+                     xycoords='data',
+                     xytext=(5.5, y),
+                     textcoords='data',
+                     arrowprops=dict(facecolor='black', shrink=0.05),
+                     horizontalalignment='left',
+                     verticalalignment='middle')
+
+    plt.tight_layout()
+    plt.savefig("main.pdf", bbox_inches="tight")
+
 
     with open("main.tex", "w") as f:
         f.write(str(sum(xs[-1] >= 10 ** -2)))
