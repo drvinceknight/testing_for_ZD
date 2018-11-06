8c740eb21e037987b9ef761a37f56c9d032dcb87
Vince Knight <vincent.knight@gmail.com>
2018-11-06 19:43:03 +0000

    Add more strategies and better results to table.

--- assets/tex/overall_summary_results/main.py
+++ assets/tex/overall_summary_results/main.py
@@ -26,42 +26,62 @@ def main(process_data=False):
         df["Name"] = player_names
         df["P(Win)"] = df["Win"] / (len(player_names) * parameters.REPETITIONS)
         df["Rank"] = df["Score"].rank(ascending=False).astype(int)
-        sserror = df["residual"].round(4)
-        df.sort_values("Score per turn", ascending=False, inplace=True)
-        df = df.round(3)
+        kappa = df["kappa"].round(4)
+        df["kappa"] = kappa
 
-        df["SSError"] = sserror
-        columns = [
-            "Rank",
-            "Name",
-            "Score per turn",
-            "P(Win)",
-            "P(CC)",
-            "P(C|CC)",
-            "P(C|CD)",
-            "P(C|DC)",
-            "P(C|DD)",
-            "SSError",
-            "alpha",
-            "beta",
-            "chi",
-        ]
+        strategies_of_interest += list(df.sort_values("kappa").head(5)["Name"])
+        strategies_of_interest += list(df.sort_values("kappa").tail(5)["Name"])
+        strategies_of_interest += list(df.sort_values("P(Win)").tail(2)["Name"])
+        strategies_of_interest += list(df.sort_values("P(CC)").tail(2)["Name"])
+        strategies_of_interest += list(df.sort_values("Score").head(5)["Name"])
+        strategies_of_interest += list(df.sort_values("Score").tail(5)["Name"])
 
-        df = df[df["Name"].isin(strategies_of_interest)][columns]
+        columns = ["Rank", "Name", "Score per turn", "P(Win)", "P(CC)", "kappa"]
+        df = df[columns]
+
         df.rename(
             columns={
                 "P(Win)": "$P($Win$)$",
                 "P(CC)": "$P(CC)$",
-                "P(C|CC)": "$P(C|CC)$",
-                "P(C|CD)": "$P(C|CD)$",
-                "P(C|DC)": "$P(C|DC)$",
-                "P(C|DD)": "$P(C|DD)$",
-                "alpha": r"$\alpha$",
-                "beta": r"$\beta$",
-                "chi": r"$\chi$",
+                "kappa": r"Overall $\kappa$",
             },
             inplace=True,
         )
+
+        per_opponent_df = pd.read_csv(
+            "../../../data/processed/full/std/per_opponent/main.csv"
+        )
+        min_kappa = per_opponent_df.groupby("Player index")["kappa"].min().abs()
+        max_kappa = per_opponent_df.groupby("Player index")["kappa"].max().abs()
+        mean_kappa = per_opponent_df.groupby("Player index")["kappa"].mean()
+        median_kappa = per_opponent_df.groupby("Player index")["kappa"].median()
+        ninety_fifth_quantile_kappa = per_opponent_df.groupby("Player index")[
+            "kappa"
+        ].quantile(0.95)
+        fifth_quantile_kappa = per_opponent_df.groupby("Player index")[
+            "kappa"
+        ].quantile(0.05)
+        std_kappa = per_opponent_df.groupby("Player index")["kappa"].std()
+        per_opponent_df = pd.DataFrame(
+            {
+                "Name": player_names,
+                "Min $\kappa$": min_kappa,
+                "5% CI $\kappa$": fifth_quantile_kappa,
+                "Median $\kappa$": median_kappa,
+                "Mean $\kappa$": mean_kappa,
+                "Std $\kappa$": std_kappa,
+                "95% CI $\kappa$": ninety_fifth_quantile_kappa,
+                "Max $\kappa$": max_kappa,
+            }
+        )
+
+        df = df.merge(per_opponent_df, on="Name")
+
+        df = df[df["Name"].isin(strategies_of_interest)]
+        df.sort_values("Score per turn", ascending=False, inplace=True)
+        df = df.round(3)
+        df = df.dropna()
+
         df.to_csv("main.csv", index=False)
     else:
         df = pd.read_csv("main.csv")
