d8e6b95a04025f77723885c4e641fca0e41955b7
Vince Knight <vincent.knight@gmail.com>
2018-12-20 11:32:17 +0000

    Clean up sse figures and include skew.

--- assets/img/sserror_in_std/main.py
+++ assets/img/sserror_in_std/main.py
@@ -3,6 +3,7 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy.stats import skew
 
 import imp
 
@@ -31,23 +32,24 @@ def main():
     summary_df = df.groupby("Player index")["Win", "Score"].sum()
 
     fig, axarr = plt.subplots(1, 2, figsize=(30, 35))
+    Y = range(1, len(players) + 1)
 
     for ax, column in zip(axarr, ("Score", "Win")):
 
-        sorted_indices = summary_df.sort_values(column, ascending=False).index
+        sorted_indices = summary_df.sort_values(column).index
         data = [df[df["Player index"] == player_index]["residual"] for player_index in sorted_indices]
 
-        ax.violinplot(data, vert=False)
-        ax.boxplot(data, vert=False, showmeans=True)
+        ax.scatter(list(map(skew, data)), Y, color="black")
+        ax.axvline(0, color="black", linestyle="--")
 
         sorted_players = [players[i].name for i in sorted_indices]
         ax.set_ylabel("Strategies")
-        ax.set_yticks(range(1, len(sorted_players) + 1))
+        ax.set_yticks(Y)
         ax.set_yticklabels(sorted_players)
         if column == "Score":
-            title = f"SSE sorted by score"
+            title = f"Skew of SSE sorted by score"
         else:
-            title = f"SSE sorted by number of wins"
+            title = f"Skew of SSE sorted by number of wins"
         ax.set_title(title, size=20)
     fig.tight_layout()
 
